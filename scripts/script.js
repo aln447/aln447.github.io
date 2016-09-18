9f5fb13d8e2d02ec01223d6c4c313b53cf952e00
aln447 <aln447@gmail.com>
2016-09-19 01:08:55 +0200

    Minor updates

--- scripts/script.js
+++ scripts/script.js
@@ -68,3 +68,20 @@ function sideScreenClose(){
     $("#text-box-inner-container").addClass("right-padding");
     $("#side-panel").addClass("right-padding");
 }
+
+
+//SMOOTH SCROLLING STOLEN FROM CSSTRICKS
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
